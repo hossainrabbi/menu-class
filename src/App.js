80f62416b90370c0ac7bada5946312ceff612c30
Hossain Rabbi <hossainrabbi2k1@gmail.com>
2022-02-14 21:17:19 +0600

    Finish menu with class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Category from './components/Category';
+import Menu from './components/Menu';
+import menu from './data';
+import { MainContainer } from './styles/Elements.style';
+import GlobalStyle from './styles/Global.style';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    // const [menuItems, setMenuItems] = useState(menu);
+    // const [categories, setCategories] = useState([]);
+    state = {
+        menuItems: menu,
+        categories: [],
+    };
+
+    // useEffect(() => {
+    //     setCategories(['all', ...new Set(menu.map((item) => item.category))]);
+    // }, [menuItems]);
+    componentDidMount() {
+        this.setState({
+            categories: ['all', ...new Set(menu.map((item) => item.category))],
+        });
+    }
+
+    filterCategory = (category) => {
+        if (category === 'all') {
+            this.setState({
+                menuItems: menu,
+            });
+            return;
+        }
+
+        this.setState({
+            menuItems: menu.filter((item) => item.category === category),
+        });
+    };
+
+    render() {
+        return (
+            <MainContainer>
+                <GlobalStyle />
+                <Category
+                    categories={this.state?.categories}
+                    filterCategory={this.filterCategory}
+                />
+                <Menu menuItems={this.state?.menuItems} />
+            </MainContainer>
+        );
+    }
 }
 
 export default App;
